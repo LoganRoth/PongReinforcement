f17285e6d65283ea93c1f1437165856febae614e
LoganRoth <logantheroth@gmail.com>
2019-11-27 16:42:38 -0500

    Cleaning up code for final submission

--- pong_sim.py
+++ pong_sim.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import numpy as np
 import matplotlib.pyplot as plot
+
 from pong_player import Human, AI, Random
 from pong_game import Game
 
@@ -27,7 +28,6 @@ def parse_args():
     parser.add_argument(
         '--watch', '-w',
         action='store_true',
-        # default=True, # Show gameplay
         default=False,
         help='If both players are AI, set if you would like to watch them '
              'play.',
@@ -35,8 +35,7 @@ def parse_args():
     parser.add_argument(
         '--train',
         action='store',
-        default=100000,
-        # default=5,
+        default=50000,
         help='Set the number of games to play to train the comupter.'
     )
     parser.add_argument(
@@ -46,67 +45,62 @@ def parse_args():
         help='Mode 0: Train two agents and have them play each other. '
              'Mode 1: Tune the algorithm parameters to find the best set. '
              'Mode 2: Train one agent and then save the Q-table to a file. '
-             'Mode 3: Two agents use saved Q-tables and play each other.'
+             'Mode 3: Two agents use saved Q-tables and play each other. '
+             'Mode 4: Train two agents and graphs results.'
     )
     args = parser.parse_args()
     return args.p1, args.p2, args.watch, int(args.train), int(args.mode)
 
-def generate_timeSteps_plot(p1_type, p2_type, watch, train, width, height):
-    """
-    Used to generate time step and hit plots.
-    """
-    alpha1 = 0.5
-    epsilon1 = 0.01
-    gamma1 = 0.8
-    alpha2 = 0.5
-    epsilon2 = 0.1
-    gamma2 = 0.8
-    if p1_type == 'AI':
-        p1 = AI('Player 1', alpha1, epsilon1, gamma1, width, height, watch)
-    elif p1_type == 'Human':
-        p1 = Human('Player 1')
-    else:
-        print('Invalid selection for P1')
-        return
-    if p2_type == 'AI':
-        p2 = AI('Player 2', alpha2, epsilon2, gamma2, width, height, watch)
-        # p2 = Random('Player 2', watch)
-    elif p2_type == 'Human':
-        p2 = Human('Player 2')
-    else:
-        print('Invalid selection for P2')
-        return
-    rando = Random('Test 1', watch)
-    rando2 = Random('Test 2', watch)
-    gameNum = 0
-    plot.plot(gameNum, 0, 'b.')
-    plot.xlabel("Game number")
-    plot.ylabel("Time steps")
-    # Loop for each episode
-    for i in range(train):
-        gameNum += 1
-        print(gameNum)
-        game = Game(width, height, p1, rando)
-        game2 = Game(width, height, rando2, p2)
-        game.playGame()
-        game2.playGame()
-        # Play a game together and count time steps
-        testGame = Game(width, height, p1, p2)
-        testGame.playGame()
-        # Can adjust this to be p1.hits, p2.hits
-        plot.plot(gameNum, testGame.timeSteps, 'b.')
 
-    plot.show()
+def write_qtable(player, width, height):
+    """
+    Writes a Q-Table text file that can be loaded in by the get_qtable function
+    and used by two agents.
+    """
+    with open('qtable.txt', 'w+') as f:
+        f.write(str(player.alpha) + '\n')
+        f.write(str(player.gamma) + '\n')
+        for x in range(width):
+            for y in range(height):
+                for idx in range(13):  # Number of paddle positions
+                    for a in range(3):  # Number of possible actions
+                        f.write(str(player.qtable[x, y, idx, a]) + '\n')
 
-    print('Done making the plot!')
+
+def get_qtable(player, name, width, height):
+    """
+    Reads a Q-Table text file create by the write_qtable function
+    and sets it as the AI agent's Q-Table to be used.
+    """
+    qtable = None
+    with open('qtable.txt', 'r') as f:
+        qtable = f.readlines()
+    qtableIter = iter(qtable)
+    player.alpha = float(next(qtableIter))
+    player.gamma = float(next(qtableIter))
+    for x in range(width):
+        for y in range(height):
+            for idx in range(13):  # Number of paddle positions
+                for a in range(3):  # Number of possible actions
+                    if name == 'Player 1':
+                        player.qtable[x, y, idx, a] = float(next(qtableIter))
+                    else:
+                        x_flip = 18 - x  # Flip the table for Player 2
+                        player.qtable[x_flip, y, idx, a] =\
+                            float(next(qtableIter))
+
 
 def train_and_play_mode(p1_type, p2_type, watch, train, width, height):
+    """
+    Mode 0
+    Train two agents and have them play each other.
+    """
     # Algorithm Parameters alpha, epsilon, gamma
-    alpha1 = 0.5
-    epsilon1 = 0.01
+    alpha1 = 0.8
+    epsilon1 = 0.005
     gamma1 = 0.8
-    alpha2 = 0.5
-    epsilon2 = 0.1
+    alpha2 = 0.8
+    epsilon2 = 0.005
     gamma2 = 0.8
     if p1_type == 'AI':
         p1 = AI('Player 1', alpha1, epsilon1, gamma1, width, height, watch)
@@ -125,14 +119,14 @@ def train_and_play_mode(p1_type, p2_type, watch, train, width, height):
         return
     rando = Random('Player 2', watch)
     # Loop for each episode
-    for i in range(train):
+    for _ in range(train):
         game = Game(width, height, p1, rando)
         game.playGame()
     print('P1 Wins: {}\nRando Wins: {}'.format(p1.wins, rando.wins))
     rando.name = 'Player 1'
     rando.wins = 0
     # Loop for each episode
-    for i in range(train):
+    for _ in range(train):
         game = Game(width, height, rando, p2)
         game.playGame()
     print('P2 Wins: {}\nRando Wins: {}'.format(p2.wins, rando.wins))
@@ -142,13 +136,17 @@ def train_and_play_mode(p1_type, p2_type, watch, train, width, height):
     p2.epsilon = -1
     p1.watch = True
     p2.watch = True
-    input("Are you ready, kids?")
+    input("Press [Enter] when ready to begin game")
     for _ in range(5):
         game = Game(width, height, p1, p2)
         game.playGame()
 
 
 def tune_mode(train, width, height):
+    """
+    Mode 1
+    Tune the algorithm parameters to find the best set.
+    """
     # Algorithm Parameters alpha, epsilon, gamma
     alphas = np.linspace(0.1, 1, 36, endpoint=False)
     epsilons = np.linspace(0.005, 0.05, 10)
@@ -179,50 +177,28 @@ def tune_mode(train, width, height):
                                                                  best_gamma))
 
 
-def write_qtable(player, width, height):
-    with open('qtable.txt', 'w+') as f:
-        f.write(str(player.alpha) + '\n')
-        f.write(str(player.gamma) + '\n')
-        for x in range(width):
-            for y in range(height):
-                for idx in range(5):  # Number of paddle positions
-                    for a in range(3):  # Number of possible actions
-                        f.write(str(player.qtable[x, y, idx, a]) + '\n')
-
-
 def train_and_save_mode(train, width, height):
-    alpha = 0.55
-    epsilon = 0.01
-    gamma = 0.9
+    """
+    Mode 2
+    Train one agent and then save the Q-table to a file.
+    """
+    alpha = 0.8
+    epsilon = 0.005
+    gamma = 0.8
     p1 = AI('Player 1', alpha, epsilon, gamma, width, height, False)
     rando = Random('Player 2', False)
     # Loop for each episode
-    for i in range(train):
+    for _ in range(train):
         game = Game(width, height, p1, rando)
         game.playGame()
     write_qtable(p1, width, height)
 
 
-def get_qtable(player, name, width, height):
-    qtable = None
-    with open('qtable.txt', 'r') as f:
-        qtable = f.readlines()
-    qtableIter = iter(qtable)
-    player.alpha = float(next(qtableIter))
-    player.gamma = float(next(qtableIter))
-    for x in range(width):
-        for y in range(height):
-            for idx in range(5):  # Number of paddle positions
-                for a in range(3):  # Number of possible actions
-                    if name == 'Player 1':
-                        player.qtable[x, y, idx, a] = float(next(qtableIter))
-                    else:
-                        x_flip = 14 - x  # Flip the table for Player 2
-                        player.qtable[x_flip, y, idx, a] =\
-                            float(next(qtableIter))
-
-
 def play_mode(p1_type, p2_type, width, height):
+    """
+    Mode 3
+    Two agents use saved Q-tables and play each other.
+    """
     if p1_type == 'AI':
         p1 = AI('Player 1', 0, -1, 0, width, height, True)
     elif p1_type == 'Human':
@@ -239,10 +215,47 @@ def play_mode(p1_type, p2_type, width, height):
         return
     get_qtable(p1, 'Player 1', width, height)
     get_qtable(p2, 'Player 2', width, height)
-    input("Are you ready, kids?")
-    for _ in range(5):
+    input("Press [Enter] when ready to begin game")
+    for _ in range(1):
         game = Game(width, height, p1, p2)
         game.playGame()
+
+
+def generate_timesteps_plot(train, width, height):
+    """
+    Mode 4
+    Used to generate time step and hit plots.
+    """
+    alpha1 = 0.8
+    epsilon1 = 0.005
+    gamma1 = 0.8
+    alpha2 = 0.8
+    epsilon2 = 0.005
+    gamma2 = 0.8
+    p1 = AI('Player 1', alpha1, epsilon1, gamma1, width, height)
+    p2 = AI('Player 2', alpha2, epsilon2, gamma2, width, height)
+    rando = Random('Test')
+    gameNum = 0
+    plot.plot(gameNum, 0, 'b.')
+    plot.xlabel("Game number")
+    plot.ylabel("Time steps")
+    # Loop for each episode
+    for _ in range(train):
+        gameNum += 1
+        print(gameNum)
+        game = Game(width, height, p1, rando)
+        game2 = Game(width, height, rando, p2)
+        game.playGame()
+        game2.playGame()
+        # Play a game together and count time steps
+        testGame = Game(width, height, p1, p2)
+        testGame.playGame()
+        # Can adjust this to be p1.hits, p2.hits
+        plot.plot(gameNum, testGame.timeSteps, 'b.')
+
+    plot.show()
+
+    print('Done making the plot!')
 
 
 def main():
@@ -261,8 +274,12 @@ def main():
         tune_mode(train, width, height)
     elif mode == 2:
         train_and_save_mode(train, width, height)
-    else:  # Mode == 3
+    elif mode == 3:
         play_mode(p1_type, p2_type, width, height)
+    elif mode == 4:
+        generate_timesteps_plot(train, width, height)
+    else:
+        print('Invalid mode selected')
 
 
 if __name__ == "__main__":
